847bb1d7f743fc8a2aaa5e573f61de07b029395e
Andre-glitch-lang <IsidoIsido2012@gmail.com>
2024-09-14 19:22:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2st program"
+print(9.99 > 9.98 and 1000 != 100.1)
+# "3rd program"
+a = (2 * 2 + 2)
+b = (2 * (2 + 2))
+print(a)
+print(b)
+print(a == b)
+# "4th program"
+print(int(float('123.456') * 10 % 10))
